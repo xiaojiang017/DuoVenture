093e85eec2289de52cbf45be6e4bb21b37957730
xiaojiang017 <134920765+xiaojiang017@users.noreply.github.com>
2023-05-29 20:21:45 +0800

    Add files via upload

---
+++ fort-end/src/common/login/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Button, Form, Input } from 'antd';
+import './index.scss'
+
+export default () => {
+
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+    return (
+        <div>
+            <div className='dv_login_conter'>
+            <div className='dv_login_form'>
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="用户名"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="密码"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit">
+                            登陆
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+            </div>
+           
+        </div>
+    )
+}
