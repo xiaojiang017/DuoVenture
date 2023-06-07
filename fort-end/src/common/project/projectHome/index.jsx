296657cea266945bedfba01b3d2ae20dcd0dc7a2
xiaojiang017 <haijiang017@gmail.com>
2023-06-07 19:29:47 +0800

    1

--- fort-end/src/common/project/projectHome/index.jsx
+++ fort-end/src/common/project/projectHome/index.jsx
@@ -1,43 +1,50 @@
-import React , {useRef , useEffect} from 'react'
-import  * as echarts from 'echarts'
+import React, { useRef, useEffect } from 'react'
+import * as echarts from 'echarts'
+import { Calendar } from 'antd';
 
 export default () => {
-    const chartRef = useRef(null); // 用于保存图表实例的引用
+  const chartRef = useRef(null); // 用于保存图表实例的引用
 
-    useEffect(() => {
-      // 创建图表实例
-      const chart = echarts.init(chartRef.current);
-      // 定义图表配置项
-      let options = {
-        title: {
+  useEffect(() => {
+    // 创建图表实例
+    const chart = echarts.init(chartRef.current);
+    // 定义图表配置项
+    let options = {
+      title: {
         //   text: "ECharts 入门示例",
+      },
+      tooltip: {},
+      legend: {
+        data: ["销量"],
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
         },
-        tooltip: {},
-        legend: {
-          data: ["销量"],
-        },
-        xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-        },
-        yAxis: {},
-        series: [
-          {
-            name: "销量",
-            type: "bar",
-            data: [5, 20, 36, 10, 10, 20],
-          },
-        ],
-      }
-      // 设置图表配置项
-      chart.setOption(options);
-      // 在组件卸载时销毁图表实例
-      return () => {
-        chart.dispose();
-      };
-    }, [])
-    return (
-        <div>
-            <div ref={chartRef} style={{ width: '100%', height: '400px' }} />
-        </div>
-    )
+      ],
+    }
+    // 设置图表配置项
+    chart.setOption(options);
+    // 在组件卸载时销毁图表实例
+    return () => {
+      chart.dispose();
+    };
+  }, [])
+  return (
+    <div className='dv_project_home_content'>
+      <div style={{width: '50%'}}>
+        <div ref={chartRef} style={{ width: '100%', height: '400px' }} />
+        <div>对话</div>
+      </div>
+      <div style={{width: '50%'}}>
+      <Calendar />
+      </div>
+    </div>
+  )
 }
